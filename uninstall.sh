0bc8829fda057ec2eecea222add63ccb5e6032af
신규승 <kyuds@kyuds.local>
2023-09-14 13:23:16 +0900

    add installation scripts

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Removing 'scripts' directory in root..."
+rm -rf ~/scripts
+
+echo "Please consider removing the PATH declaration for the 'scripts' directory..."
+
+echo "Thank you for using"
+
